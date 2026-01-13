22f3ea885e56caa407e5f6a1932c3bc170ef5a4a
mfelig <mfelig@gmail.com>
2026-01-13 10:53:18 +0200

    created Scope class

--- src/ex5/model/Scope.java
+++ src/ex5/model/Scope.java
@@ -1,4 +1,27 @@
 package ex5.model;
 
+import java.util.HashMap;
+
 public class Scope {
+    private final Scope parentScope;
+    private final HashMap<String, Variable> localVariables;
+    public Scope(Scope parentScope) {
+        this.parentScope = parentScope;
+        this.localVariables = new HashMap<>();
+    }
+
+    public void declareVariable(Variable variable) {
+        localVariables.put(variable.getName(), variable);
+    }
+
+    public boolean containsVariable(String name) {
+        return localVariables.containsKey(name);
+    }
+    public Variable getVariable(String name) {
+        return localVariables.get(name);
+    }
+
+    public Scope getParentScope() {
+        return parentScope;
+    }
 }
--- src/ex5/model/Variable.java
+++ src/ex5/model/Variable.java
@@ -1,4 +1,36 @@
 package ex5.model;
 
 public class Variable {
+    private final String type;
+    private String name;
+    private boolean isInitialized = false;
+    private final boolean isFinal;
+
+    public Variable(String type, String name, boolean isFinal) {
+        this.type = type;
+        this.name = name;
+        this.isFinal = isFinal;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public  String getName() {
+        return name;
+    }
+
+    public boolean isInitialized() {
+        return isInitialized;
+    }
+
+    public boolean isFinal() {
+        return isFinal;
+    }
+
+    public void assignVal(){
+        this.isInitialized = true;
+    }
+
+
 }
