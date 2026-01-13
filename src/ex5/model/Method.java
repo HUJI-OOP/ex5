d3ac6b1227cc3fdf58c11270d1e26bb3aab66692
mfelig <mfelig@gmail.com>
2026-01-13 10:58:21 +0200

    created Method class

--- src/ex5/model/Method.java
+++ src/ex5/model/Method.java
@@ -1,4 +1,38 @@
 package ex5.model;
 
+import java.util.List;
+
+/**
+ * Method class
+ * wrapper of a method
+ * @author Eilam Soroka, Maayan felig
+ */
 public class Method {
+    private String name;
+    private final List<Variable> parameters;
+
+    /**
+     * constructor of method
+     * @param name
+     * @param parameters
+     */
+    public Method(String name, List<Variable> parameters) {
+        this.name = name;
+        this.parameters = parameters;
+    }
+
+    /**
+     * get name
+     * @return String name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * get parameters
+     * @return List<Variable> parameters
+     */
+    public List<Variable> getParameters() {
+        return parameters;
+    }
 }
